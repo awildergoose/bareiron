be912e75eff6e39e1b0ef3d2b0b6f79b80e7d546
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-09-16 21:49:24 +0300

    chore: improve command handler

--- include/commands.h
+++ include/commands.h
@@ -1,7 +1,6 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-#include "globals.h"
 #include <stddef.h>
 
 typedef struct {
@@ -9,10 +8,21 @@ typedef struct {
   size_t count;
 } Command;
 
+typedef int (*CommandCallback)(Command cmd, int client_fd);
+
+typedef struct {
+  const char *name;
+  int arg_count;
+  CommandCallback callback;
+} RegisteredCommand;
+
 Command parse_command(char *input);
 void free_command(Command *cmd);
+int handle_command(Command cmd, int client_fd);
+
 int str_to_int(const char *str, int *out);
 int str_to_float(const char *str, float *out);
-int handle_command(Command cmd, int client_fd);
+int startswith(const char *str, const char *prefix);
+int invalid_arguments(int client_fd);
 
 #endif
--- src/commands.c
+++ src/commands.c
@@ -7,6 +7,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "commands.h"
+#include "globals.h"
+#include "packetbuilder.h"
+#include "packets.h"
+#include "procedures.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 Command parse_command(char *input) {
   Command cmd = {0};
   size_t capacity = 4;
@@ -52,11 +61,11 @@ int str_to_float(const char *str, float *out) {
 
 int startswith(const char *str, const char *prefix) {
   if (!str || !prefix)
-    return false;
+    return 0;
   size_t len_prefix = strlen(prefix);
   size_t len_str = strlen(str);
   if (len_prefix > len_str)
-    return false;
+    return 0;
   return strncmp(str, prefix, len_prefix) == 0;
 }
 
@@ -66,39 +75,60 @@ int invalid_arguments(int client_fd) {
   return 1;
 }
 
-int handle_command(Command cmd, int client_fd) {
-  if (cmd.count == 0)
-    return 1;
+int cmd_gamemode(Command cmd, int client_fd) {
+  if (cmd.count != 2)
+    return invalid_arguments(client_fd);
+
+  int gamemode = GAMEMODE_SURVIVAL;
+  if (startswith(cmd.args[1], "c"))
+    gamemode = GAMEMODE_CREATIVE;
+  if (startswith(cmd.args[1], "sp"))
+    gamemode = GAMEMODE_SPECTATOR;
+  if (startswith(cmd.args[1], "a"))
+    gamemode = GAMEMODE_ADVENTURE;
+
   PlayerData *player;
   if (getPlayerData(client_fd, &player))
     return 1;
 
-  if (strcmp(cmd.args[0], "gamemode") == 0) {
-    int gamemode = GAMEMODE_SURVIVAL;
-    if (cmd.count != 2)
-      return invalid_arguments(client_fd);
-    if (startswith(cmd.args[1], "c"))
-      gamemode = GAMEMODE_CREATIVE;
-    if (startswith(cmd.args[1], "sp"))
-      gamemode = GAMEMODE_SPECTATOR;
-    if (startswith(cmd.args[1], "a"))
-      gamemode = GAMEMODE_ADVENTURE;
+  sc_gameEvent(client_fd, GAME_EVENT_CHANGE_GAME_MODE, gamemode);
+  player->gamemode = gamemode;
+  return 0;
+}
 
-    sc_gameEvent(client_fd, GAME_EVENT_CHANGE_GAME_MODE, gamemode);
-    player->gamemode = gamemode;
-  } else if (strcmp(cmd.args[0], "endtest") == 0) {
-    PacketBuilder *pb = pb_create(128);
-    pb_writeByte(pb, GAME_EVENT_BEGIN_RAINING);
-    pb_writeFloat(pb, 0);
-    pb_send(pb, client_fd, 0x22);
-    pb_reset(pb);
-    pb_writeByte(pb, GAME_EVENT_RAIN_LEVEL_CHANGE);
-    pb_writeFloat(pb, 1);
-    pb_send(pb, client_fd, 0x22);
-    pb_free(pb);
-  } else {
-    printf("Unknown command: %s\n", cmd.args[0]);
+int cmd_endtest(Command cmd, int client_fd) {
+  PacketBuilder *pb = pb_create(128);
+  pb_writeByte(pb, GAME_EVENT_BEGIN_RAINING);
+  pb_writeFloat(pb, 0);
+  pb_send(pb, client_fd, 0x22);
+
+  pb_reset(pb);
+  pb_writeByte(pb, GAME_EVENT_RAIN_LEVEL_CHANGE);
+  pb_writeFloat(pb, 1);
+  pb_send(pb, client_fd, 0x22);
+
+  pb_free(pb);
+  return 0;
+}
+
+RegisteredCommand commands[] = {{"gamemode", 1, cmd_gamemode},
+                                {"endtest", 0, cmd_endtest},
+                                {NULL, 0, NULL}};
+
+int handle_command(Command cmd, int client_fd) {
+  if (cmd.count == 0)
+    return 1;
+
+  for (int i = 0; commands[i].name != NULL; i++) {
+    if (strcmp(cmd.args[0], commands[i].name) == 0) {
+      int expected = commands[i].arg_count;
+      if (expected >= 0 && (int)(cmd.count - 1) != expected)
+        return invalid_arguments(client_fd);
+
+      return commands[i].callback(cmd, client_fd);
+    }
   }
 
-  return 0;
+  printf("Unknown command: %s\n", cmd.args[0]);
+  return 1;
 }
